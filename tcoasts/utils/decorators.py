e37907cf6d83679100ffdf605836ab9600831054
Josue-Martinez-Moreno <josue.martinezmoreno@anu.edu.au>
2019-10-14 23:49:53 +1100

    Warning bug

--- tcoasts/utils/decorators.py
+++ tcoasts/utils/decorators.py
@@ -8,7 +8,7 @@ def _file_exists(func):
     def wrap(self,*args, **kwargs):
         if os.path.isfile(self.tmpfile):
             warnings.filterwarnings('default',module='tcoasts')
-            warnings.warn('Loading previous saved data.', Warning,module='TransportAlongCoast')
+            warnings.warn('Loading previous saved data.', Warning)
             interp=xr.open_dataset('./tmp_interp_transects.nc')
             self.interp_data=interp.load()
         else:
